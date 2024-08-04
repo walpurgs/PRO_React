9873701b3fcf46e67e80edb2df773df807d5ec01
Ales <valesya555@mail.ru>
2024-08-04 22:41:21 +0700

    ADD: jest + first test

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,7 +1,8 @@
 module.exports = {
 	env: {
 		browser: true,
-		es2021: true
+		es2021: true,
+		jest: true
 	},
 	extends: ["plugin:react/recommended", "airbnb", "plugin:i18next/recommended"],
 	parser: "@typescript-eslint/parser",
